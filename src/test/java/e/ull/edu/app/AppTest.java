415fed4798d6d73787bc8bbb7e2de306162b203c
dagoull <dcastell@ull.es>
2022-11-23 18:02:26 +0000

    Init..

---
+++ src/test/java/e/ull/edu/app/AppTest.java
@@ -0,0 +1,20 @@
+package e.ull.edu.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
